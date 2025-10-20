7ca18087944c6acbb5e8bda17969ab4ed8dda5bf
amaury <amauryj99@gmail.com>
2025-10-20 20:48:30 +0200

    question7 finished

--- TP/TP3/src/main.py
+++ TP/TP3/src/main.py
@@ -33,22 +33,22 @@ import cv2
 # plt.show()
 
 #QUESTION4
-img = func.open_image("../Images_TP/Profondeur.png")
-result = func.focus_two_zones(img,
-                         zone1_center=0.70, zone1_width=0.50, z1_top=0.0, z1_bottom=1,
-                         zone2_center=0.40, zone2_width=0.15, z2_top=0.3, z2_bottom=1)
+# img = func.open_image("../Images_TP/Profondeur.png")
+# result = func.focus_two_zones(img,
+#                          zone1_center=0.70, zone1_width=0.50, z1_top=0.0, z1_bottom=1,
+#                          zone2_center=0.40, zone2_width=0.15, z2_top=0.3, z2_bottom=1)
 
-plt.figure(figsize=(10,5))
-plt.subplot(1,2,1)
-plt.title("Image originale")
-plt.imshow(img)
-plt.axis('off')
+# plt.figure(figsize=(10,5))
+# plt.subplot(1,2,1)
+# plt.title("Image originale")
+# plt.imshow(img)
+# plt.axis('off')
 
-plt.subplot(1,2,2)
-plt.title("Effet de profondeur de champ")
-plt.imshow(result)
-plt.axis('off')
-plt.show()
+# plt.subplot(1,2,2)
+# plt.title("Effet de profondeur de champ")
+# plt.imshow(result)
+# plt.axis('off')
+# plt.show()
 
 #QUESTION5
 # eiffel = func.open_image("../Images_TP/tour-eiffel.jpg", "RGB")
