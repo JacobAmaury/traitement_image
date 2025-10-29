c6b452f7f1f2d8f43b03e2c50dff7f7bbba57e63
amaury_grosPC <amauryj99@gmail.com>
2025-10-29 15:09:25 +0100

    need to debug

--- Projet/src/func.py
+++ Projet/src/func.py
@@ -21,7 +21,6 @@ def p_tild(image ,mode=0, alpha=50, omega=0.05):
     Gy, Gx = np.gradient(image)
     g = np.sqrt(Gx**2 + Gy**2)
 
-    p = np.empty_like(image)
     if mode == 0 :
         p = np.exp(-alpha*(g**2))
     else:
@@ -29,7 +28,6 @@ def p_tild(image ,mode=0, alpha=50, omega=0.05):
     p_tild = omega + p
 
     return p_tild
-
 
 
 def is_pix(x, y, max_x, max_y):
@@ -47,23 +45,47 @@ def pix_neighbours(x, y, max_x, max_y):
     if is_pix(x,y+1, max_x, max_y): neighbours.append((y+1,x))
     return neighbours
 
-def solve_eikonal(Us, pix_y, pix_x, P_tilds):
-    return 0
+def solve_eikonal(Us, pix_x, pix_y, P_tilds):
+    h,w = Us.shape
+    p_tild = P_tilds[pix_y,pix_x]
+
+    if is_pix(pix_x-1, pix_y, w, h) and is_pix(pix_x+1, pix_y, w, h):
+        a = min(Us[pix_y, pix_x-1],Us[pix_y, pix_x+1])
+    else:
+        if is_pix(pix_x-1, pix_y, w, h):
+            a = Us[pix_y, pix_x-1]
+        else :
+            a = Us[pix_y, pix_x+1]
+
+    if is_pix(pix_x, pix_y-1, w, h) and is_pix(pix_x, pix_y+1, w, h):
+        b = min(Us[pix_y-1, pix_x],Us[pix_y-1, pix_x])
+    else:
+        if is_pix(pix_x, pix_y-1, w, h):
+            b = Us[pix_y-1, pix_x]
+        else :
+            b = Us[pix_y+1, pix_x]
+
+    if abs(a - b) >= p_tild:
+        U_new = min(a, b) + p_tild
+    else:
+        U_new = (a + b + np.sqrt(2 * p_tild ** 2 - (a - b) ** 2)) / 2.0
+    return U_new
+
+
+
 
 def fast_marching(P_tilds, init_point_x,init_point_y):
     """This function compute the fast marching matching methode. 
         take P_tild (can be compute with the functio P_tild) and the coordinate of the initial point"""
     #initilisation
     max_y, max_x = P_tilds.shape
-    Us = np.empty_like(P_tilds)
-    Us = np.inf
-    Us[init_point_y, init_point_x] = 0
+    Us = np.full_like(P_tilds, np.inf)
+    Us[init_point_y, init_point_x] = 0.
 
-    labels = np.empty_like(P_tilds) #FAR=0, TRIAL=1 and ALIVE = 2 
     FAR = 0
     TRIAL = 1
     ALIVE = 2
-    labels = FAR 
+    labels = np.full_like(P_tilds, FAR) #FAR=0, TRIAL=1 and ALIVE = 2 
     labels[init_point_y, init_point_x] = TRIAL
 
     heaps = [(init_point_y, init_point_x)] #list of trial points
@@ -85,7 +107,7 @@ def fast_marching(P_tilds, init_point_x,init_point_y):
         for neighbour_y, neighbour_x in neighbours:
 
             if labels[neighbour_y,neighbour_x] != ALIVE:
-                current_U = solve_eikonal(Us, min_pix[0], min_pix[1], P_tilds)
+                current_U = solve_eikonal(Us, min_pix[1], min_pix[0], P_tilds)
 
                 if current_U < Us[min_pix[0],min_pix[1]]:
                     Us[min_pix[0],min_pix[1]] = current_U
@@ -94,12 +116,26 @@ def fast_marching(P_tilds, init_point_x,init_point_y):
                         labels[neighbour_y,neighbour_x] = TRIAL
 
     return Us
-                
-
-
-        
 
 
 
+def backtrack(Us, start_x, start_y, end_x, end_y, step, max_it):
+    paths = [(end_x, end_y)]
+    current_x = end_x
+    current_y = end_y
+    avoid_0_div = 1e-6
+    cpt = 0
+    while not((current_x == start_x) and (current_y == start_y)) or (cpt < max_it):
+        dUx = (Us[current_y, current_x+1]-Us[current_y, current_x])
+        dUy = (Us[current_y+1, current_x]-Us[current_y, current_x])
+        norm_grad = np.sqrt(dUx**2+dUy**2+avoid_0_div)
+        normalized_dUx = dUx/norm_grad
+        normalized_dUy = dUy/norm_grad
 
+        current_x -= step * normalized_dUx
+        current_y -= step * normalized_dUy
+
+        paths.append((int(current_x+0.5), int(current_y+0.5)))
+        cpt += 1
+    return paths
 
--- Projet/src/test_func.py
+++ Projet/src/test_func.py
@@ -1,12 +1,23 @@
 import func
 import matplotlib.pyplot as plt
 
+
+start_x = 218
+start_y = 328
+
+end_x = 171
+end_y = 97
+
 image_path = ["../images/1.png", "../images/2.png", "../images/3.png"]
 
 image = func.open_image("../images/1.png", "L")
 image_pretraited = func.pretreatment(image)
-P_tilde_mode0 = func.p_tild(image_pretraited, 0, alpha=80, sigma=1.5, omega=0.1)
-P_tilde_mode1 = func.p_tild(image_pretraited, 1,alpha=80, sigma=1.5, omega=0.1)
+P_tilde_mode0 = func.p_tild(image_pretraited, 0, alpha=80, omega=0.1)
+P_tilde_mode1 = func.p_tild(image_pretraited, 1,alpha=80, omega=0.1)
+
+
+Us = func.fast_marching(P_tilde_mode0, start_x, start_y)
+paths = func.backtrack(Us, start_x, start_y, end_x, end_y, 0.5, 5000)
 
 
 plt.subplot(2,3,1)
@@ -16,12 +27,6 @@ plt.imshow(image, cmap="gray")
 plt.subplot(2,3,2)
 plt.title("image pretraited")
 plt.imshow(image_pretraited*255, cmap="gray")
+plt.plot(paths)
 
-plt.subplot(2,3,3)
-plt.title("P_tilde mode 0")
-plt.imshow(P_tilde_mode0*255, cmap="gray")
-
-plt.subplot(2,3,4)
-plt.title("P_tilde mode 1")
-plt.imshow(P_tilde_mode1*255, cmap="gray")
 plt.show()
