22a66c2a2424185604f835ddb62298779774e045
amaury_grosPC <amauryj99@gmail.com>
2025-10-29 19:28:10 +0100

    code is woking need to do a menu

--- Projet/src/func.py
+++ Projet/src/func.py
@@ -13,21 +13,23 @@ def open_image(path, mode="RGB"):
 def pretreatment(image):
     """In this function we normalize and denoise the image"""
     image = image/np.max(image)
-    image = cv.GaussianBlur(image,(3,3),0)
+    image = cv.GaussianBlur(image,(5,5),0)
     return image
 
-def p_tild(image ,mode=0, alpha=50, omega=0.05):
-    """This function compute p_tild. if mode = 0 P = exp(-alpha * g^2) else P = 1 / (1 + (g/sigma)^2)"""
+def p_tild(image, mode=0, alpha=50, sigma=1.0, omega=0.05):
+    """Compute p_tild.
+       if mode=0: P = exp(-alpha*g^2)
+       else: P = 1 / (1 + (g/sigma)^2)
+    """
     Gy, Gx = np.gradient(image)
     g = np.sqrt(Gx**2 + Gy**2)
 
-    if mode == 0 :
-        p = np.exp(-alpha*(g**2))
+    if mode == 0:
+        P = np.exp(-alpha * (g ** 2))
     else:
-        p = 1/(1+alpha*g**2)
-    p_tild = omega + p
-
-    return p_tild
+        P = 1 / (1 + (g / sigma) ** 2)
+    P_tild = omega + P
+    return P_tild
 
 
 def is_pix(x, y, max_x, max_y):
@@ -45,7 +47,9 @@ def pix_neighbours(x, y, max_x, max_y):
     if is_pix(x,y+1, max_x, max_y): neighbours.append((y+1,x))
     return neighbours
 
-def solve_eikonal(Us, pix_x, pix_y, P_tilds):
+
+
+def solve_eikonal(Us, pix_y, pix_x, P_tilds):
     h,w = Us.shape
     p_tild = P_tilds[pix_y,pix_x]
 
@@ -58,7 +62,7 @@ def solve_eikonal(Us, pix_x, pix_y, P_tilds):
             a = Us[pix_y, pix_x+1]
 
     if is_pix(pix_x, pix_y-1, w, h) and is_pix(pix_x, pix_y+1, w, h):
-        b = min(Us[pix_y-1, pix_x],Us[pix_y-1, pix_x])
+        b = min(Us[pix_y-1, pix_x],Us[pix_y+1, pix_x])
     else:
         if is_pix(pix_x, pix_y-1, w, h):
             b = Us[pix_y-1, pix_x]
@@ -71,51 +75,46 @@ def solve_eikonal(Us, pix_x, pix_y, P_tilds):
         U_new = (a + b + np.sqrt(2 * p_tild ** 2 - (a - b) ** 2)) / 2.0
     return U_new
 
-
-
-
-def fast_marching(P_tilds, init_point_x,init_point_y):
-    """This function compute the fast marching matching methode. 
-        take P_tild (can be compute with the functio P_tild) and the coordinate of the initial point"""
-    #initilisation
+def fast_marching(P_tilds, init_point_x, init_point_y):
     max_y, max_x = P_tilds.shape
     Us = np.full_like(P_tilds, np.inf)
-    Us[init_point_y, init_point_x] = 0.
+    Us[init_point_y, init_point_x] = 0.0
 
     FAR = 0
     TRIAL = 1
     ALIVE = 2
-    labels = np.full_like(P_tilds, FAR) #FAR=0, TRIAL=1 and ALIVE = 2 
+
+    labels = np.full_like(P_tilds, FAR, dtype=int)
     labels[init_point_y, init_point_x] = TRIAL
 
-    heaps = [(init_point_y, init_point_x)] #list of trial points
-
+    heaps = [(init_point_y, init_point_x)]
 
     while len(heaps) != 0:
-        min_value = np.inf
-        min_pix = (0,0)
-
-        for (point_y,point_x) in heaps:
-            if Us[point_y,point_x] < min_value:
-                min_value = Us[point_y, point_x] 
-                min_pix =(point_y, point_x)
+        # Trouve le pixel de U minimal
+        min_val = np.inf
+        min_pix = None
+        for (point_y, point_x) in heaps:
+            if Us[point_y, point_x] < min_val:
+                min_val = Us[point_y, point_x]
+                min_pix = (point_y, point_x)
+        
+        if min_pix is None: break
 
         heaps.remove(min_pix)
-        labels[min_pix[0], min_pix[1]] = ALIVE
+        labels[min_pix] = ALIVE
 
-        neighbours = pix_neighbours(min_pix[1], min_pix[0], max_x, max_y)
-        for neighbour_y, neighbour_x in neighbours:
-
-            if labels[neighbour_y,neighbour_x] != ALIVE:
-                current_U = solve_eikonal(Us, min_pix[1], min_pix[0], P_tilds)
-
-                if current_U < Us[min_pix[0],min_pix[1]]:
-                    Us[min_pix[0],min_pix[1]] = current_U
-                    if labels[neighbour_y,neighbour_x] == FAR:
-                        heaps.append((neighbour_y,neighbour_x))
-                        labels[neighbour_y,neighbour_x] = TRIAL
+        for ny, nx in pix_neighbours(min_pix[1], min_pix[0], max_x, max_y):
+            if labels[ny, nx] != ALIVE:
+                U_new = solve_eikonal(Us, ny, nx, P_tilds)
+                if U_new < Us[ny, nx]:
+                    Us[ny, nx] = U_new
+                    if labels[ny, nx] == FAR:
+                        heaps.append((ny, nx))
+                        labels[ny, nx] = TRIAL
 
     return Us
+
+
 
 
 
@@ -125,9 +124,10 @@ def backtrack(Us, start_x, start_y, end_x, end_y, step, max_it):
     current_y = end_y
     avoid_0_div = 1e-6
     cpt = 0
-    while not((current_x == start_x) and (current_y == start_y)) or (cpt < max_it):
-        dUx = (Us[current_y, current_x+1]-Us[current_y, current_x])
-        dUy = (Us[current_y+1, current_x]-Us[current_y, current_x])
+    while not((current_x == start_x) and (current_y == start_y)) and (cpt < max_it):
+        dUx = Us[int(current_y), int(current_x+1)] - Us[int(current_y), int(current_x)]
+        dUy = Us[int(current_y+1), int(current_x)] - Us[int(current_y), int(current_x)]
+
         norm_grad = np.sqrt(dUx**2+dUy**2+avoid_0_div)
         normalized_dUx = dUx/norm_grad
         normalized_dUy = dUy/norm_grad
@@ -135,7 +135,7 @@ def backtrack(Us, start_x, start_y, end_x, end_y, step, max_it):
         current_x -= step * normalized_dUx
         current_y -= step * normalized_dUy
 
-        paths.append((int(current_x+0.5), int(current_y+0.5)))
+        paths.append((int(current_x), int(current_y)))
         cpt += 1
     return paths
 
